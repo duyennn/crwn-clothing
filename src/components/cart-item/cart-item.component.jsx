a749b1a8572fddb6abf5404c5fcad781fadd78b4
Justracy <justracy@Duyens-MacBook-Pro.local>
2020-03-09 15:54:43 -0400

    Fixed dropdown item image. Update our application with reselect, replaced cart selector with memoized selectors.

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './cart-item.styles.scss';
 
-const CartItem = ({ item: { imageURL, price, name, quantity } }) => (
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
     <div className='cart-item'>
-        <img src={imageURL} alt='item' />
+        <img src={imageUrl} alt='item' />
         <div className='item-details'>
             <span className='name'>{name}</span>
             <span className='price'>{quantity} x ${price}</span>
